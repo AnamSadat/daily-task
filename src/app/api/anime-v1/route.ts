2317958ed8dfb0e435efc1a9861360da05212b25
AnamSadat <anamsadat3@gmail.com>
2025-07-15 13:39:22 +0700

    add method post api

--- src/app/api/anime-v1/route.ts
+++ src/app/api/anime-v1/route.ts
@@ -1,8 +1,37 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import { anime } from "@/lib/data"
+import { NewAnime, NewAnimeSchema } from "@/type/type"
 
 // TODO: api anime
 
 export async function GET() {
-  return Response.json(anime, { status: 200 })
+  const respone = {
+    status: 200,
+    data: anime
+  }
+  return NextResponse.json(respone)
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json()
+
+  const parse = NewAnimeSchema.safeParse(body)
+
+  if (!parse.success) {
+    return NextResponse.json({ error: parse.error.format() }, { status: 400 });
+  }
+
+  const newAnime: NewAnime = {
+    ...parse.data,
+    id: Date.now()
+  }
+
+  anime.push(newAnime)
+
+  const respone = {
+    status: 200,
+    data: newAnime,
+  }
+
+  return NextResponse.json(respone)
 }
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,10 +1,12 @@
-export const anime = [
+import { NewAnime } from "@/type/type";
+
+export const anime: NewAnime[] = [
   {
     id: 1,
     nama: 'Attack on Titan',
     status: 'Completed',
     skor: 9.1,
-    durasi: '24',
+    durasi: 24,
     genre: 'Aksi, Fantasi Gelap, Militer',
     img_url: 'https://4kwallpapers.com/images/walls/thumbs_3t/21075.jpg',
     deskripsi: 'Manusia hidup di dalam kota yang dikelilingi tembok besar untuk melindungi diri dari Titan, makhluk raksasa pemakan manusia. Eren Yeager bersumpah untuk memusnahkan semua Titan setelah kampung halamannya dihancurkan dan ibunya terbunuh.'
@@ -14,7 +16,7 @@ export const anime = [
     nama: 'Jujutsu Kaisen',
     status: 'Completed',
     skor: 8.7,
-    durasi: '23',
+    durasi: 23,
     genre: 'Aksi, Horor, Supernatural',
     img_url: 'https://4kwallpapers.com/images/walls/thumbs_3t/19746.jpg',
     deskripsi: 'Yuji Itadori menelan jimat terkutuk untuk menyelamatkan temannya, dan menjadi wadah bagi Raja Kutukan, Sukuna. Ia kemudian bergabung dengan Jujutsu Sorcerers untuk melawan kutukan dan mencari sisa-sisa Sukuna.'
@@ -24,7 +26,7 @@ export const anime = [
     nama: 'Spy x Family',
     status: 'Completed',
     skor: 8.5,
-    durasi: '24',
+    durasi: 24,
     genre: 'Komedi, Aksi, Slice of Life',
     img_url: 'https://4kwallpapers.com/images/walls/thumbs_3t/11194.png',
     deskripsi: 'Seorang mata-mata, seorang pembunuh, dan seorang esper membentuk keluarga palsu untuk menjalankan misi rahasia, tanpa mengetahui identitas asli satu sama lain.'
@@ -34,7 +36,7 @@ export const anime = [
     nama: 'One Piece',
     status: 'Ongoing',
     skor: 9.0,
-    durasi: '24',
+    durasi: 24,
     genre: 'Petualangan, Fantasi, Komedi',
     img_url: 'https://4kwallpapers.com/images/walls/thumbs_3t/22064.jpg',
     deskripsi: 'Monkey D. Luffy, seorang anak laki-laki yang tubuhnya menjadi karet setelah makan Buah Iblis, memulai perjalanan untuk menemukan harta karun legendaris One Piece dan menjadi Raja Bajak Laut.'
@@ -44,7 +46,7 @@ export const anime = [
     nama: 'Kimetsu no Yaiba',
     status: 'Completed',
     skor: 8.6,
-    durasi: '23',
+    durasi: 23,
     genre: 'Aksi, Fantasi, Sejarah',
     img_url: 'https://4kwallpapers.com/images/walls/thumbs_3t/22988.jpg',
     deskripsi: 'Tanjiro Kamado, seorang anak laki-laki yang keluarganya dibantai oleh iblis dan adik perempuannya, Nezuko, diubah menjadi iblis, memulai perjalanan untuk membalas dendam dan mencari cara untuk mengembalikan adiknya menjadi manusia.'
